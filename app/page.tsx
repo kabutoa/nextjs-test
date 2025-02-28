de42f257c53277569b746d99872b9d911c04f61f
kabutoa <yinyun957@163.com>
2025-02-28 15:52:42 +0800

    测试env部署

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div>
       <h1>{process.env.NEXT_PUBLIC_SITE_NAME || '环境变量未生效'}</h1>
-      <p className="text-red-400">Hello World</p>
+      <p className="text-red-400 font-extrabold">Hello World</p>
     </div>
   );
 }
