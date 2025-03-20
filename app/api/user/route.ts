bc7a8f226b7221b41d47c23c1b930a11692b793d
kabutoa <yinyun957@163.com>
2025-03-20 15:44:06 +0800

    fix: deploy failed

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,7 @@
 import {prisma} from '@/prisma/prsima'
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest): Promise<NextResponse> {
+export async function GET(): Promise<NextResponse> {
   try {
     const users = await prisma.user.findMany()
     return NextResponse.json(users, {status: 200})
